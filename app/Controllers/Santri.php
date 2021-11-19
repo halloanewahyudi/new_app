a80acda442f0643f70629770b6fd60a91c0b17bf
halloanewahyudi <30697981+halloanewahyudi@users.noreply.github.com>
2021-11-19 13:45:40 +0700

    form yang sama dengan steping

--- app/Controllers/Santri.php
+++ app/Controllers/Santri.php
@@ -3,11 +3,105 @@
 namespace App\Controllers;
 
 use App\Controllers\BaseController;
+use App\Models\SantriModel;
 
 class Santri extends BaseController
 {
+
+    public function __construct()
+    {
+        $this->db = \Config\Database::connect();
+        $this->santri_model = new SantriModel();
+        helper('form','auth','file');
+    }
+
     public function index()
     {
         //
+    }
+
+    public function view ($id){
+
+    }
+
+    public function create(){
+        $d =[
+            'judul'=>'Isi Profil Santri',
+            'action'=> base_url('steping/santri-action'),
+        ];
+       return view('santri/form-santri',$d);
+    }
+
+    public function create_action(){
+
+        $this->santri_model->insert([
+            'nisn'     => $this->request->getVar('nisn'),
+            'nik'   => $this->request->getVar('nik'),
+            'kontak'   => $this->request->getVar('kontak'),
+            'golongan_darah'   => $this->request->getVar('golongan_darah'),
+            'anak_ke'   => $this->request->getVar('anak_ke'),
+            'status_anak'   => $this->request->getVar('status_anak'),
+            'tinggi_badan'   => $this->request->getVar('tinggi_badan'),
+            'berat_badan'   => $this->request->getVar('berat_badan'),
+            'status_tinggal'   => $this->request->getVar('status_tinggal'),
+            'pembiayaan'   => $this->request->getVar('pembiayaan'),
+            'alamat'   => $this->request->getVar('alamat'),
+            'desa_kelurahan'   => $this->request->getVar('desa_kelurahan'),
+            'kecamatan'   => $this->request->getVar('kecamatan'),
+            'kabupaten_kota'   => $this->request->getVar('kabupaten_kota'),
+            'provinsi'   => $this->request->getVar('provinsi'),
+            'kode_pos'   => $this->request->getVar('kode_pos'),
+            'negara'   => $this->request->getVar('negara'),
+            'hobi'   => $this->request->getVar('hobi'),
+            'cita_cita'   => $this->request->getVar('cita_cita'),
+            'jumlah_hafalan'   => $this->request->getVar('jumlah_hafalan'),
+            'user_id'   => user_id(),
+        ]);
+        session()->setFlashdata('message', 'Data Santri telah di input');
+        return  redirect()->to('santri/create-santri');
+
+    }
+
+    public function edit($id){
+
+        $model = $this->santri_model->where('id',$id)->first();
+        $d =[
+            'judul'=>'Edit Profil Santri',
+            'action'=> base_url('santri/edit-santri-action/'.$id),
+            'data_santri'=>$model,
+        ];
+       return view('santri/form-santri',$d);
+
+    }
+
+    public function edit_action($id){
+
+        $this->santri_model->update($id, [
+            'nisn'     => $this->request->getVar('nisn'),
+            'nik'   => $this->request->getVar('nik'),
+            'kontak'   => $this->request->getVar('kontak'),
+            'golongan_darah'   => $this->request->getVar('golongan_darah'),
+            'anak_ke'   => $this->request->getVar('anak_ke'),
+            'status_anak'   => $this->request->getVar('status_anak'),
+            'tinggi_badan'   => $this->request->getVar('tinggi_badan'),
+            'berat_badan'   => $this->request->getVar('berat_badan'),
+            'status_tinggal'   => $this->request->getVar('status_tinggal'),
+            'pembiayaan'   => $this->request->getVar('pembiayaan'),
+            'alamat'   => $this->request->getVar('alamat'),
+            'desa_kelurahan'   => $this->request->getVar('desa_kelurahan'),
+            'kecamatan'   => $this->request->getVar('kecamatan'),
+            'kabupaten_kota'   => $this->request->getVar('kabupaten_kota'),
+            'provinsi'   => $this->request->getVar('provinsi'),
+            'kode_pos'   => $this->request->getVar('kode_pos'),
+            'negara'   => $this->request->getVar('negara'),
+            'hobi'   => $this->request->getVar('hobi'),
+            'cita_cita'   => $this->request->getVar('cita_cita'),
+            'jumlah_hafalan'   => $this->request->getVar('jumlah_hafalan'),
+            'user_id'   => user_id(),
+        ]);
+
+        session()->setFlashdata('message','Data Santri Berhasil di Update');
+        return  redirect()->to('santri/edit-santri/'.$id);
+
     }
 }
--- app/Controllers/SekolahAsal.php
+++ app/Controllers/SekolahAsal.php
@@ -3,11 +3,73 @@
 namespace App\Controllers;
 
 use App\Controllers\BaseController;
+use App\Models\SantriModel;
+use App\Models\SekolahAsalModel;
 
 class SekolahAsal extends BaseController
 {
+    public function __construct()
+    {
+        $this->db = \Config\Database::connect();
+        $this->santri_model = new SantriModel();
+        $this->sekolah_asal_model = new SekolahAsalModel();
+        helper('form','auth','file');
+    }
+    
     public function index()
     {
         //
+    }
+
+    public function create(){
+        $d =[
+            'judul'=>'Isi Sekolah Asal',
+            'action'=> base_url('santri/sekolah-asal-action'),
+        ];
+       return view('sekolah-asal/form-sekolah-asal',$d);
+    }
+
+    public function create_action(){
+        $this->sekolah_asal_model->insert([
+            'sekolah_asal'     => $this->request->getVar('sekolah_asal'),
+            'npsn'   => $this->request->getVar('npsn'),
+            'tahun_lulus'   => $this->request->getVar('tahun_lulus'),
+            'alamat'   => $this->request->getVar('alamat'),
+            'desa_kelurahan'   => $this->request->getVar('desa_kelurahan'),
+            'kecamatan'   => $this->request->getVar('kecamatan'),
+            'kabupaten_kota'   => $this->request->getVar('kabupaten_kota'),
+            'provinsi'   => $this->request->getVar('provinsi'),
+            'kode_pos'   => $this->request->getVar('kode_pos'),
+            'user_id'   => user_id(),
+        ]);
+          session()->setFlashdata('message','Sekolah asal telah di input');
+         return  redirect()->to('santri/create-sekolah-asal');
+    }
+
+    public function edit($id){
+        $model = $this->sekolah_asal_model->where('id',$id)->first();
+        $d =[
+            'judul'=>'Edit Sekolah Asal',
+            'action'=> base_url('santri/edit-sekolah-asal-action/'.$id),
+            'data_sekolah_asal'=>$model,
+        ];
+       return view('sekolah-asal/form-sekolah-asal',$d);
+    }
+
+    public function edit_action($id){
+        $this->sekolah_asal_model->update($id, [
+            'sekolah_asal'     => $this->request->getVar('sekolah_asal'),
+            'npsn'   => $this->request->getVar('npsn'),
+            'tahun_lulus'   => $this->request->getVar('tahun_lulus'),
+            'alamat'   => $this->request->getVar('alamat'),
+            'desa_kelurahan'   => $this->request->getVar('desa_kelurahan'),
+            'kecamatan'   => $this->request->getVar('kecamatan'),
+            'kabupaten_kota'   => $this->request->getVar('kabupaten_kota'),
+            'provinsi'   => $this->request->getVar('provinsi'),
+            'kode_pos'   => $this->request->getVar('kode_pos'),
+            'user_id'   => user_id(),
+        ]);
+          session()->setFlashdata('message','Sekolah asal telah update');
+        return  redirect()->to('santri/edit-sekolah-asal/'.$id);
     }
 }
