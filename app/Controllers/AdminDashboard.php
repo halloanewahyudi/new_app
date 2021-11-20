a339b2835762d5f5bed646c35834e7d46a52e606
halloanewahyudi <30697981+halloanewahyudi@users.noreply.github.com>
2021-11-20 10:19:19 +0700

    admindashboard

--- app/Controllers/AdminDashboard.php
+++ app/Controllers/AdminDashboard.php
@@ -1,13 +1,143 @@
 <?php
-
 namespace App\Controllers;
-
 use App\Controllers\BaseController;
+use Myth\Auth\Models\UserModel;
+use App\Models\SantriModel;
+use App\Models\SekolahAsalModel;
+use App\Models\BerkasModel;
+use App\Models\PembayaranModel;
+use App\Models\WaliMuridModel;
+use App\Models\UndanganModel;
 
 class AdminDashboard extends BaseController
 {
-    public function index()
+    
+    public function __construct()
     {
-        return 'ini dsahboard Admin';
+        $this->user_model = new UserModel();
+        $this->santri_model = new SantriModel();
+        $this->sekolah_asal_model = new SekolahAsalModel();
+        $this->wali_murid_model = new WaliMuridModel();
+        $this->berkas_model = new BerkasModel();
+        $this->pembayaran_model = new PembayaranModel();
+        $this->undangan_model = new UndanganModel();
+        helper(['form', 'url','auth']);
+
+         $this->db = \Config\Database::connect();
     }
+
+    public function index(){
+    
+        $d=[
+        'judul'=>'',
+        'total_pendaftar'=> $this->total_user(),
+        'total_santri'=>$this->total_santri(),
+
+        'total_ra'=>$this->total_jenjang('SDIT'),
+        'total_mi'=>$this->total_jenjang('SMPIT Putra'),
+        'total_mts_putra'=>$this->total_jenjang('SMPIT Putri'),
+        'total_mts_putri'=>$this->total_jenjang('SMAIT Putra'),
+        'total_ma_putra'=>$this->total_jenjang('SMAIT Putri'),
+        'total_ma_putri'=>$this->total_jenjang('TAKHOSUS'),
+
+        'total_pembayaran'=>$this->total_pembayaran(),
+        'total_verifikasi_pembayaran'=>$this->total_verifikasi_pembayaran(),
+
+        'total_pembayaran_ra'=>$this->total_pembayaran_by_jenjang('SDIT'),
+        'total_pembayaran_mi'=>$this->total_pembayaran_by_jenjang('SMPIT Putra'),
+        'total_pembayaran_mts_putra'=>$this->total_pembayaran_by_jenjang('SMPIT Putri'),
+        'total_pembayaran_mts_putri'=>$this->total_pembayaran_by_jenjang('SMAIT Putra'),
+        'total_pembayaran_ma_putra'=>$this->total_pembayaran_by_jenjang('SMAIT Putri'),
+        'total_pembayaran_ma_putri'=>$this->total_pembayaran_by_jenjang('TAKHOSUS'),
+
+        'total_verifikasi_ra'=> $this->total_verifikasi_pembayaran_by_jenjang('SDIT'),
+        'total_verifikasi_mi'=> $this->total_verifikasi_pembayaran_by_jenjang('SMPIT Putra'),
+        'total_verifikasi_mts_putra'=> $this->total_verifikasi_pembayaran_by_jenjang('SMPIT Putri'),
+        'total_verifikasi_mts_putri'=> $this->total_verifikasi_pembayaran_by_jenjang('SMAIT Putra'),
+        'total_verifikasi_ma_putra'=> $this->total_verifikasi_pembayaran_by_jenjang('SMAIT Putri'),
+        'total_verifikasi_ma_putri'=> $this->total_verifikasi_pembayaran_by_jenjang('TAKHOSUS'),
+        'last_login'=>$this->last_login(),
+        'total_berkas'=> $this->total_berkas(),
+    ];
+
+     return view('admin/dashboard',$d);
+    
+     
+    }
+
+    function total_user(){
+     $data_user = $this->user_model->countAllResults();
+     return  $data_user;
+    }
+
+    function total_santri(){
+     $data_santri = $this->santri_model
+     ->select('user_id')
+     ->groupBy('user_id')
+     ->countAllResults();
+     return $data_santri;
+    }
+
+    function total_jenjang($jenjang ){
+    	$data_santri = $this->santri_model
+        ->select('user_id')
+        ->join('users','users.id = santri.user_id','right')
+        ->groupBy('user_id')
+        ->where('users.educational_level',$jenjang)
+        ->countAllResults();
+        return $data_santri;
+    }
+
+    function total_pembayaran(){
+     $data_pembayaran = $this->pembayaran_model
+     ->groupBy('user_id')
+     ->countAllResults();
+     return $data_pembayaran;
+    }
+
+    function total_verifikasi_pembayaran(){
+     $verifikasi_pembayaran = $this->pembayaran_model
+     ->where('status',1)->countAllResults();
+     return $verifikasi_pembayaran;
+    }
+
+    function total_pembayaran_by_jenjang($jenjang){
+     $data_pembayaran = $this->pembayaran_model
+     ->select('user_id')
+     ->join('users','users.id = pembayaran.user_id')
+     ->groupBy('user_id')
+     ->where('users.educational_level',$jenjang)
+     ->countAllResults();
+     return $data_pembayaran;
+    }
+
+    function total_verifikasi_pembayaran_by_jenjang($jenjang){
+     $verifikasi_pembayaran = $this->pembayaran_model
+     ->select('user_id')
+     ->join('users','users.id = pembayaran.user_id')
+     ->where('users.educational_level',$jenjang)
+     ->where('pembayaran.status',1)->countAllResults();
+     return $verifikasi_pembayaran;
+    }
+
+    function last_login(){
+        $model = $this->db->table('auth_logins')
+        ->orderBy('id','DESC')
+        ->get()->getFirstRow();
+        $ip = $model->ip_address; 
+        $email = $model->email;
+        $time = $model->date;
+        return 'IP :'. $ip .'<br> Email:'. $email .'<br> waktu: '.$time;
+    }
+
+    function total_berkas(){
+      $model = $this->berkas_model
+      ->select('berkas.user_id')
+      ->join('users','users.id = berkas.user_id')
+      ->groupBy('berkas.user_id')
+      ->orderBy('users.id','DESC')
+      ->countAllResults();
+       return $model;
+    }
+
 }
--- app/Views/admin/dashboard.php
+++ app/Views/admin/dashboard.php
@@ -1,41 +1,146 @@
 <?= $this->extend('layouts/admin-layout'); ?>
 <?= $this->Section('content'); ?>
-<div id="dashboard">
-    <div class="col-lg-12">
-        <div class="p-5 bg-white">
-            <div class="container">
-                <h4 class="display-6">Pendaftaran Santri Al Andalus</h4>
-                <p class="lead">Tahun Ajaran 2021/2022</p>
-                <div class="row">
-                    <div class="col-md-3">
-                        <div class="border rounded p-2 text-center">
-                            <i data-feather="users"></i> <br>
-                            Calon santri <br> yang mendaftar <br>
-                            <span class="fs-3"> <?= $total_daftar; ?></span>
+<div id="adminDashboard">
+	<div class="row equal">
+		<div class="col-lg-8 col-md-6">
+			<div class="card">
+				<div class="card-body">
+					<h3 class="display-4">Pendaftaran calon santri</h1>
+					<p class="lead">tahun ajaran 2022/2023</p>
+				</div>
+			</div>
+		</div>
+		<div class="col-lg-4 col-md-6">
+			<div class="card">
+				<div class="card-body">
+					<span>Total Pendaftar: <?= $total_pendaftar; ?></span><br>
+					<span>Total Santri (mengisi form): <?= $total_santri; ?></span> <br>
+                       <?php if( in_groups(4,true) || in_groups(1,true) ): ?>
+                    <a href="<?= base_url('ExportSantri')?>" class="btn btn-sm btn-outline-primary"> Download Santri <span data-feather="download"></span></a>
+                   <?php endif; ?>
+					<hr>
+					<span>Total membayar: <?= $total_pembayaran; ?></span><br>
+					<span>Total terverifikasi: <?= $total_verifikasi_pembayaran; ?></span>
+                    <?php if( in_groups(3,true) || in_groups(1,true) ): ?>
+                    <a href="<?= base_url('ExportBayar')?>" class="btn btn-sm btn-outline-primary"> Download Pembayaran <span data-feather="download"></span></a>
+                <?php endif; ?>
+					<hr>
+						Login terakhir:  <br>
+					 <?= $last_login; ?>
+				</div>
+			</div>
+		</div>
+	</div><!--END ROW-->
+	<div class="row equal">
+		<div class="col-md-3">
+                <div class="card border-start border-0 border-warning border-4 shadow">
+                    <div class="card-body ">
+                        <div class="d-flex justify-content-between align-items-center">
+                            <div>
+                                <i data-feather="users"></i><br>
+                                <span>Calon Santri</span><br>
+                                <span>MTs Putra</span>
+                            </div>
+                            <h2><?= $total_mts_putra; ?> <small>Siswa</small></h2>
                         </div>
-                    </div>
-                    <div class="col-md-3">
-                        <div class="border rounded p-2 text-center">
-                            <i data-feather="credit-card"></i> <br>
-                            Calon santri <br> yang membayar <br>
-                            <span class="fs-3"> <?= $total_pembayaran; ?></span>
+                        <hr>
+                        <div class="d-flex justify-content-between">
+                            <span>Sudah bayar</span><span><?= $total_pembayaran_mts_putra; ?></span>
+                        </div>
+                        <div class="d-flex justify-content-between">
+                            <span>Terverifikasi</span><span><?= $total_verifikasi_mts_putra; ?></span>
                         </div>
                     </div>
                 </div>
-
-            </div>
-        </div>
-
-    </div>
+            </div><!-- end col-3-->
+            <div class="col-md-3">
+                <div class="card border-start border-0 border-primary border-4 shadow">
+                    <div class="card-body ">
+                        <div class="d-flex justify-content-between align-items-center">
+                            <div>
+                                <i data-feather="users"></i><br>
+                                <span>Calon Santri</span><br>
+                                <span>MTs Putri</span>
+                            </div>
+                            <h2><?= $total_mts_putri; ?> <small>Siswa</small></h2>
+                        </div>
+                        <hr>
+                        <div class="d-flex justify-content-between">
+                            <span>Sudah bayar</span><span><?= $total_pembayaran_mts_putri; ?></span>
+                        </div>
+                        <div class="d-flex justify-content-between">
+                            <span>Terverifikasi</span><span><?= $total_verifikasi_mts_putri; ?></span>
+                        </div>
+                    </div>
+                </div>
+            </div><!-- end col-3-->
+            <div class="col-md-3">
+                <div class="card border-start border-0 border-success border-4 shadow">
+                    <div class="card-body ">
+                        <div class="d-flex justify-content-between align-items-center">
+                            <div>
+                                <i data-feather="users"></i><br>
+                                <span>Calon Santri</span><br>
+                                <span>MA Putra</span>
+                            </div>
+                            <h2><?= $total_ma_putra; ?> <small>Siswa</small></h2>
+                        </div>
+                        <hr>
+                        <div class="d-flex justify-content-between">
+                            <span>Sudah bayar</span><span><?= $total_pembayaran_ma_putra; ?></span>
+                        </div>
+                        <div class="d-flex justify-content-between">
+                            <span>Terverifikasi</span><span><?= $total_verifikasi_ma_putra; ?></span>
+                        </div>
+                    </div>
+                </div>
+            </div><!-- end col-3-->
+             <div class="col-md-3">
+                <div class="card border-start border-0 border-success border-4 shadow">
+                    <div class="card-body ">
+                        <div class="d-flex justify-content-between align-items-center">
+                            <div>
+                                <i data-feather="users"></i><br>
+                                <span>Calon Santri</span><br>
+                                <span>MA Putri</span>
+                            </div>
+                            <h2><?= $total_ma_putri; ?> <small>Siswi</small></h2>
+                        </div>
+                        <hr>
+                        <div class="d-flex justify-content-between">
+                            <span>Sudah bayar</span><span><?= $total_pembayaran_ma_putri; ?></span>
+                        </div>
+                        <div class="d-flex justify-content-between">
+                            <span>Terverifikasi</span><span><?= $total_verifikasi_ma_putri; ?></span>
+                        </div>
+                    </div>
+                </div>
+            </div><!-- end col-3-->
+	</div><!--END ROW-->
+	<div class="row">
+		<div class="col-lg-6">
+			<div class="p-2 bg-white mb-3 rounded">
+				<div class="d-flex justify-content-around align-items-center">
+					<span>Calon Santri RA</span>
+					<span class="lead"> <?= $total_ra?></span>
+					<span>Sudah bayar: <?= $total_pembayaran_ra; ?></span>
+					<span>Terverifikasi: <?= $total_verifikasi_ra; ?></span>
+			   </div>
+		   </div>
+		   <div class="p-2 bg-white mb-3 rounded">
+				<div class="d-flex justify-content-around align-items-center">
+					<span>Calon Santri MI</span>
+					<span class="lead"> <?= $total_mi?></span>
+					<span>Sudah bayar: <?= $total_pembayaran_mi; ?></span>
+					<span>Terverifikasi: <?= $total_verifikasi_mi; ?></span>
+			   </div>
+		   </div>
+		</div>
+		<div class="col-lg-6">
+			<span data-feather="layers"></span> Yang sudah upload Berkas 
+			<?= $total_berkas ?> santri
+		</div>
+	</div>
 </div>
-<script type="text/javascript">
-    const v_dashboard = new Vue({
-        el: '#dashboard',
-        data() {
-            return {
-                yang_daftar: '',
-            }
-        }
-    });
-</script>
-<?= $this->endSection(); ?>
+
+<?=  $this->endSection();?>
